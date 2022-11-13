612b90c2f0720db495ce8b0d8031e5d1bcb0dcfc
Yan Kaiky <yankaikys@gmail.com>
2022-11-13 04:06:04 -0300

    feat(ContinentsService): add yup on project

--- src/app/services/continents.service.ts
+++ src/app/services/continents.service.ts
@@ -1,10 +1,22 @@
+import * as yup from 'yup';
+
 interface IDataContinent {
   name: string;
 }
 
+const schema: yup.SchemaOf<IDataContinent> = yup.object().shape({
+  name: yup.string().required().min(3),
+});
+
 class ContinentsService {
   create = async (payload: IDataContinent) => {
-    return payload;
+    try {
+      const validation = schema.validate(payload);
+
+      return validation;
+    } catch (error) {
+      return error as yup.ValidationError;
+    }
   };
 }
 
