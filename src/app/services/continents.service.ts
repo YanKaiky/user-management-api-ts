e7a0aae434061adbd06797981dc3b5e8ae004ef9
Yan Kaiky <yankaikys@gmail.com>
2022-11-13 21:51:10 -0300

    feat(ContinentsService): add prisma

--- src/app/services/continents.service.ts
+++ src/app/services/continents.service.ts
@@ -1,18 +1,25 @@
+import { PrismaClient } from '@prisma/client';
 import * as yup from 'yup';
 
-interface IDataContinent {
-  name: string;
-}
+const prisma = new PrismaClient();
 
 const schema: yup.SchemaOf<IDataContinent> = yup.object().shape({
   name: yup.string().required().min(3),
 });
 
+interface IDataContinent {
+  name: string;
+}
+
 class ContinentsService {
   create = async (payload: IDataContinent) => {
-    const validation = schema.validate(payload);
+    const validation = await schema.validate(payload);
 
-    return validation;
+    const continent = await prisma.continents.create({
+      data: validation,
+    });
+
+    return continent;
   };
 }
 
