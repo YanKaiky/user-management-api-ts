50c0661010ff0fdad0fb0108ff6c0922e5e658d7
Yan Kaiky <yankaikys@gmail.com>
2022-11-19 16:59:13 -0300

    feat(App): add cors

--- app.ts
+++ app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './src/routes';
 import 'dotenv/config';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
